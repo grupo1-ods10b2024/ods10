1301ac5c63791029c1bbf3945c0cc709582b1426
grupo1-ods10b2024 <gruposilva07@gmail.com>
2024-10-08 20:11:45 -0300

    Update files
    Faq editado

--- js.js
+++ js.js
@@ -187,3 +187,43 @@ document.querySelectorAll(".botao").forEach((botao) => {
     });
   });
 });
+
+// JS para acordeon da página faq:
+
+$(function () {
+  var Accordion = function (el, multiple) {
+    this.el = el || {};
+    this.multiple = multiple || false;
+    var links = this.el.find(".article-title");
+    links.on(
+      "click",
+      {
+        el: this.el,
+        multiple: this.multiple,
+      },
+      this.dropdown
+    );
+  };
+
+  Accordion.prototype.dropdown = function (e) {
+    var $el = e.data.el;
+    ($this = $(this)), ($next = $this.next());
+    $next.slideToggle();
+    $this.parent().toggleClass("open");
+    if (!e.data.multiple) {
+      $el
+        .find(".accordion-content")
+        .not($next)
+        .slideUp()
+        .parent()
+        .removeClass("open");
+    }
+  };
+  var accordion = new Accordion($(".accordion-container"), false);
+});
+
+$(document).on("click", function (event) {
+  if (!$(event.target).closest("#accordion").length) {
+    $this.parent().toggleClass("open");
+  }
+});
